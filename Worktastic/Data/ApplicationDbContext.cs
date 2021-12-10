ca529011376930f61d992ab1ae558fa6ea5d09c0
Manuel Brunner <manuelbr2488@gmail.com>
2021-12-10 20:52:24 +0100

    Projektdateien hinzufügen.

---
+++ Worktastic/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Worktastic.Models;
+
+namespace Worktastic.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<JobPosting> JobPostings { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
