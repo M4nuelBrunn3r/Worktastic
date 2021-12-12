37ecb133d4f867d2f419e71b1bfa5b6ab3140b9d
Manuel Brunner <manuelbr2488@gmail.com>
2021-12-12 23:03:05 +0100

    Add save job posting.

--- Worktastic/Controllers/JobPostingController.cs
+++ Worktastic/Controllers/JobPostingController.cs
@@ -5,12 +5,20 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Worktastic.Data;
 using Worktastic.Models;
 
 namespace Worktastic.Controllers
 {
     public class JobPostingController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public JobPostingController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -33,7 +41,33 @@ namespace Worktastic.Controllers
                 }
             }
 
-            // TOTO: write job posting to db
+            if (jobPosting.Id == 0)
+            {
+                // Add new job if not editing
+                _context.JobPostings.Add(jobPosting);
+            }
+            else
+            {
+                var jobFromDb = _context.JobPostings.SingleOrDefault(x => x.Id == jobPosting.Id);
+
+                if (jobFromDb == null)
+                {
+                    return NotFound();
+                }
+
+                jobFromDb.CompanyImage = jobPosting.CompanyImage;
+                jobFromDb.CompanyName = jobPosting.CompanyName;
+                jobFromDb.ContactMail = jobPosting.ContactMail;
+                jobFromDb.ContactPhone = jobPosting.ContactPhone;
+                jobFromDb.ContactWebsite = jobPosting.ContactWebsite;
+                jobFromDb.Description = jobPosting.Description;
+                jobFromDb.JobLocation = jobPosting.JobLocation;
+                jobFromDb.JobTitle = jobPosting.JobTitle;
+                jobFromDb.Salary = jobPosting.Salary;
+                jobFromDb.StartDate = jobPosting.StartDate;
+            }
+
+            _context.SaveChanges();
 
             return RedirectToAction(nameof(Index));
         }
