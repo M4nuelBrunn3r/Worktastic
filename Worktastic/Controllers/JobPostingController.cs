588407bc6cb40e6ee46d12e031c024de6bce35ce
Manuel Brunner <manuelbr2488@gmail.com>
2021-12-27 20:38:49 +0100

    Add that only logged in users can edit their own job postings.

--- Worktastic/Controllers/JobPostingController.cs
+++ Worktastic/Controllers/JobPostingController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -10,6 +11,7 @@ using Worktastic.Models;
 
 namespace Worktastic.Controllers
 {
+    [Authorize]
     public class JobPostingController : Controller
     {
         private readonly ApplicationDbContext _context;
@@ -30,6 +32,12 @@ namespace Worktastic.Controllers
             if (id != 0)
             {
                 var jobPostingFromDb = _context.JobPostings.SingleOrDefault(x => x.Id == id);
+
+                if (jobPostingFromDb.OwnerUsername != User.Identity.Name)
+                {
+                    return Unauthorized();
+                }
+
                 if (jobPostingFromDb != null)
                 {
                     return View(jobPostingFromDb);
